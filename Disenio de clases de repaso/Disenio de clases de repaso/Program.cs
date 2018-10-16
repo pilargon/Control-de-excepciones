bbb0185818d492fb23d60e416b18effbac5be14d
Pilar G <pilargonzalezcla@gmail.com>
2018-10-16 17:30:53 +0200

    final

---
+++ Disenio de clases de repaso/Disenio de clases de repaso/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disenio_de_clases_de_repaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo r1 = new Rectangulo(5,6);
+            int area=r1.Area();
+            Console.WriteLine("El area del rectangulo es: " + area);
+            
+            double diagonal = r1.Diagonal();
+            Console.WriteLine("La diagonal del rectangulo es: " + diagonal);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Disenio de clases de repaso/Disenio de clases de repaso/Rectangulo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disenio_de_clases_de_repaso
+{
+    class Rectangulo
+    {
+        private int largo;
+        private int ancho;
+
+        public Rectangulo()
+        {
+
+        }
+        public Rectangulo(int largo, int ancho)
+        {
+            this.largo = largo;
+            this.ancho = ancho;
+        }
+        ////Métodos selectores (get) y modificadores (set)
+        //public String GetSelectores()
+        //{
+        //    return;
+        //}
+        //public void SetModificadores(String nombre)
+        //{
+        //    this.nombre = nombre;
+        //}
+        //Un método llamado area que devuelve el área del rectángulo
+        public int Area()
+        {
+            return largo * ancho;
+        }
+        //Un método llamado diagonal que devuelve el valor de la diagonal ( con decimales)
+        public double Diagonal()
+        {
+            return Math.Pow(largo,2)+Math.Pow(ancho,2);
+        }
+
+
+
+
+
+    }
+}
